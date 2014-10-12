e2ed004b186d169df9c6affcc15200f3cbe0ae2b
Melinda Seckington <mseckington@gmail.com>
2014-10-12 04:15:55 +0100

    Add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Company.delete_all
+Evangelist.delete_all
+
 Company.create(id: 1, name: "PayPal", twitter: "PayPalDev")
+Company.create(id: 2, name: "Twilio", twitter: "twilio")
 
 Evangelist.create([
   {
@@ -15,5 +19,19 @@ Evangelist.create([
     company_id: 1,
     points: 0,
     image: "https://pbs.twimg.com/profile_images/490595754268827649/VlSgDg_r_400x400.jpeg"
-  }
+  },
+  {
+    fullname: "Michael Wawra",
+    twitter: "xmjw",
+    company_id: 2,
+    points: 0,
+    image: "https://pbs.twimg.com/profile_images/378800000306982590/9a353903fa4e6f5857240221f9142b4c_400x400.jpeg"
+  },
+  {
+    fullname: "Joe Nash",
+    twitter: "jan_sh",
+    company_id: 1,
+    points: 0,
+    image: "https://pbs.twimg.com/profile_images/510436387959472128/d4IKagvh_400x400.jpeg"
+  },
 ])
